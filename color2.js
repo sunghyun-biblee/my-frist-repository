af4ba2f95f9c367373e7abe8b9d96d9dc65f97aa
sunghyun-biblee <110729319+sunghyun-biblee@users.noreply.github.com>
2022-08-18 20:57:12 +0900

    라이브러리 (Jquery)
    라이브러리에 대해 알아보고 Jquery를 사용해보았다

---
+++ color2.js
@@ -0,0 +1,41 @@
+var Links={
+  setColor:function (color) {
+  //   var alist = document.querySelectorAll('a');
+  //   var i = 0;
+  //   while (i < alist.length) {
+  //     console.log(alist[i]);
+  //     alist[i].style.color = color;
+  //     i = i + 1;
+  //
+  // }
+  //$('a') ->이 웹페이지에있는 모든태그들을 제이쿼리 제어하겠다.
+  $('a').css('color',color);
+}
+}
+
+var Body={
+  setColor:function (color){
+    //document.querySelector('body').style.color = color;
+    $('body').css('color',color);
+  },
+  setBackgroundColor:function (color){
+    //document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor',color);
+  }
+}
+function nightdayhandler(self) {
+  var target = document.querySelector('body')
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black')
+    Body.setColor('white');
+    self.value = 'day';
+
+    Links.setColor('powderblue');
+  } else {
+    Body.setBackgroundColor('white')
+    Body.setColor('black');
+    self.value = 'night';
+
+    Links.setColor('blue');
+  }
+}
